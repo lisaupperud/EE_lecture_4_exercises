8548c9c58690eea1d93946ab5623094e4b3c7a58
liluppis <lisa.upp@hotmail.com>
2025-10-15 16:49:14 +0200

    initial commit

---
+++ src/main/java/com/liluppis/EE_4_exercises/Ee4ExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.liluppis.EE_4_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ee4ExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ee4ExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liluppis/EE_4_exercises/Ee4ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liluppis.EE_4_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ee4ExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
