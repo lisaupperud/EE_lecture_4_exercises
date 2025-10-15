363eee244066a436a1e627d47e1391a65f079bc3
liluppis <lisa.upp@hotmail.com>
2025-10-15 17:42:39 +0200

    add: preparations for Flyway

--- src/main/java/com/liluppis/EE_4_exercises/message/Message.java
+++ src/main/java/com/liluppis/EE_4_exercises/message/Message.java
@@ -1,13 +1,17 @@
 package com.liluppis.EE_4_exercises.message;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 @Table("messages")
 public record Message(
+        @Id
         Long id,
         String message,
+        @Column("created_at")
         LocalDateTime createdAt
 ) {
 }
